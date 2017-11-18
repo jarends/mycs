20e95817d1a5d0648d12b59a3240f06bd9d3199b
Jan-Olaf Arends <jan.arends@definitely.eu>
2017-11-18 12:41:15 +0100

    index added

---
+++ index.js
@@ -0,0 +1,13 @@
+(function()
+{
+    module.exports = {
+        CmdMap:     require('./js/cmd-map'),
+        Dispatcher: require('./js/cmd-map'),
+        getBounds:  require('./js/cmd-map'),
+        KeyInfo:    require('./js/cmd-map'),
+        Rect:       require('./js/cmd-map'),
+        Store:      require('./js/cmd-map'),
+        utils:      require('./js/cmd-map'),
+    };
+
+}).call(this);
